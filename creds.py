69d261b108cf666b8d9b1947540d6e9a309d1d5c
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-07-07 06:30:53 +0530

    ❣️

--- creds.py
+++ creds.py
@@ -6,8 +6,8 @@ import os
 
 class Credentials:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")  # from @botfather
-    API_ID = int(os.environ.get("API_ID"))  # from https://my.telegram.org/apps
-    API_HASH = os.environ.get("API_HASH")  # from https://my.telegram.org/apps
+    API_ID = int(os.environ.get("API_ID"))  # from https://my.telegram.org/apps or @MT_MyTelegramOrg_Bot
+    API_HASH = os.environ.get("API_HASH")  # from https://my.telegram.org/apps or @MT_MyTelegramOrg_Bot
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS","").split())
 
