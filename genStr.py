5479573440534f754de029961ff20a8ccd8ccff2
harshil8981 <69214079+harshil8981@users.noreply.github.com>
2022-01-22 22:36:57 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import time
-from bot import bot, HU_APP
+from bot import bot, HP_APP
 from pyromod import listen
 from asyncio.exceptions import TimeoutError
 
@@ -21,7 +21,7 @@ I'm **String Session Generator** \nI Can Generate Pyrogram's String Session Of Y
 
 Now Send Your `API_ID` Same As `APP_ID` To Start Generating Session.
 
-Get API_ID from **@MT_MyTelegramOrg_Bot** How To Get UserSession For Website\**nhttps://youtu.be/WUN_12-dYOM**"""
+Get API_ID from **@TeleORG_Bot** How To Get UserSession For Website\**nhttps://t.me/Hpbot_update**"""
 HASH_TEXT = "Now Send Your `API_HASH`.\n\nGet API_HASH From https://my.telegram.org\n\nPress /cancel to Cancel Task."
 PHONE_NUMBER_TEXT = (
     "Now Send Your Telegram Account's Phone Number in International Format. \n"
@@ -30,14 +30,14 @@ PHONE_NUMBER_TEXT = (
 )
 
 
-UPDATES_CHANNEL = os.environ.get('UPDATES_CHANNEL', 'AsmSafone')
+UPDATES_CHANNEL = os.environ.get('UPDATES_CHANNEL', 'Hpbot_update)
 
 @bot.on_message(filters.private & filters.command("start"))
 async def genStr(_, msg: Message):
     if msg.chat.id in Credentials.BANNED_USERS:
         await bot.send_message(
             chat_id=msg.chat.id,
-            text="You are Banned. Contact My [Support Group](https://t.me/safothebot)",
+            text="You are Banned. Contact My [Support Group](https://t.me/Hpbot_update)",
             reply_to_message_id=msg.message_id
         )
         return
@@ -49,7 +49,7 @@ async def genStr(_, msg: Message):
             if user.status == "kicked":
                await bot.send_message(
                    chat_id=msg.chat.id,
-                   text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/safothebot).",
+                   text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/Hpbot_update).",
                    parse_mode="markdown",
                    disable_web_page_preview=True
                )
@@ -71,7 +71,7 @@ async def genStr(_, msg: Message):
         except Exception:
             await bot.send_message(
                 chat_id=msg.chat.id,
-                text="**Something Went Wrong. Contact My [Support Group](https://t.me/safothebot).**",
+                text="**Something Went Wrong. Contact My [Support Group](https://t.me/HP_Bot_discuss_group).**",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
